f1ed6204a29c5ade090a40b0ccee0f7b0777f4fa
Max Silva <64077276+maxssilva@users.noreply.github.com>
2021-02-26 10:38:41 -0300

    Update ClienteRepository.java
    Descrição da usabilidade da classe ClienteRepository

--- zup-cadastro-clientes/src/main/java/com/zup/zupcadastroclientes/repository/ClienteRepository.java
+++ zup-cadastro-clientes/src/main/java/com/zup/zupcadastroclientes/repository/ClienteRepository.java
@@ -1,9 +1,11 @@
+//Classe responsável por interagir com o banco de dados, cria, salva, busca, edita um cliente no banco de dados
+
 package com.zup.zupcadastroclientes.repository;
 
 import com.zup.zupcadastroclientes.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository //anotação que especifica que essa classe é do tipo repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
